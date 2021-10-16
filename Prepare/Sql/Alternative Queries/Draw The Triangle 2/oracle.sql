e560d1ad9ff8e1fe7cedb679c8031a0f5261f9c9
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2021-10-16 17:57:37 +0600

    Create oracle.sql

---
+++ Prepare/Sql/Alternative Queries/Draw The Triangle 2/oracle.sql
@@ -0,0 +1,24 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+set serveroutput on;
+
+declare
+    star clob;
+begin
+    for i in 1..20 loop
+        star := '';
+        for j in 1..i loop
+            star := star || '*' || ' ';
+        end loop;
+        dbms_output.put_line(star);
+    end loop;
+end;
+/
