fa8e4361a0dc8d04837ef2edbc60789570594601
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2022-02-13 16:44:07 +0600

    Create oracle.sql

---
+++ Prepare/Sql/Basic Select/Weather Observation Station 8/oracle.sql
@@ -0,0 +1,13 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+select distinct city
+from station
+where 
+    substr(city, 1, 1) IN ('A', 'E', 'I', 'O', 'U')
+    and
+    substr(city, -1, 1) IN ('a', 'e', 'i', 'o', 'u')
+order by city asc
+;
