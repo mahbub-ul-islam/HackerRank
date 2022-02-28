f2df60acb91571afccef17cfc7041c2a8f2f1984
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2022-02-28 16:47:42 +0600

    Create oracle.sql

---
+++ Prepare/Sql/Basic Select/Higher Than 75 Marks/oracle.sql
@@ -0,0 +1,10 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+select name
+from students
+where marks > 75
+order by substr(name, -3), id asc
+;
