8249989262fc363a6895910e2f9c8290d01247d0
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2022-03-05 16:50:39 +0600

    Create oracle.sql

---
+++ Prepare/Sql/Basic Select/Employee Names/oracle.sql
@@ -0,0 +1,9 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+select name
+from employee
+order by name asc
+;
