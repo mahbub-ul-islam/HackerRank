89fbb5f6c8b84439766978633a6d04101640149a
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2022-03-10 16:53:27 +0600

    Create oracle.sql

---
+++ Prepare/Sql/Basic Join/Population Census/oracle.sql
@@ -0,0 +1,15 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+
+
+select 
+    sum(ci.population)
+from 
+    CITY ci, 
+    COUNTRY co
+where 
+    co.continent = 'Asia' and
+    co.code = ci.countrycode;
