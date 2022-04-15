8dbf1ab4118e92df947a11d084a44f831ad12449
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2022-04-15 17:04:05 +0600

    Create main.java

---
+++ Prepare/Java/Introduction/Java Static Initializer Block/main.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        
+        while(sc.hasNext()) {
+            int b = sc.nextInt();
+            int h = sc.nextInt();
+            
+            if (b < 0 || h < 0) {
+                System.out.println("java.lang.Exception: Breadth and height must be positive");
+            }
+            else{
+                System.out.println(b * h);
+            }
+        }
+    }
+}
