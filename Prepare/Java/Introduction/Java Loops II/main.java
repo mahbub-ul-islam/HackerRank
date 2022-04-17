f610b07f74add4c9d49131fe565392976473dd8f
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2022-04-17 17:05:02 +0600

    Create main.java

---
+++ Prepare/Java/Introduction/Java Loops II/main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            for (int j=0; j<n; j++) {
+                int output = a;
+                for (int k=0; k<j+1; k++) {
+                    output += (b * Math.pow(2, k));
+                }
+                System.out.print(output + " ");
+            }
+            
+            System.out.println();
+        }
+        in.close();
+    }
+}
